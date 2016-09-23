bdef3358fa6decf781e302f929c9f0c5b0c960af
Robert Faulk <robertfaulk@utexas.edu>
2016-09-23 17:01:01 -0500

    added pathogen to addons

--- get_addons.sh
+++ get_addons.sh
@@ -1,3 +1,6 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim || exit 1
+
 cd ~/.vim/bundle || exit 1
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/scrooloose/nerdtree.git
