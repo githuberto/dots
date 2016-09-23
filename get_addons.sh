118477f83a4337c9eefe1c4dbfa1a9a6c4fa37f8
Robert Faulk <robertfaulk@utexas.edu>
2016-09-23 16:58:39 -0500

    added get addons script

---
+++ get_addons.sh
@@ -0,0 +1,5 @@
+cd ~/.vim/bundle || exit 1
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/tpope/vim-fugitive.git
