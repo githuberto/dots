c1f88c40ccd66ea974aea00711742d62c3824091
Robert Faulk <robertgfaulk@gmail.com>
2019-07-21 22:02:17 -0700

    More instructions and fixed packages.sh

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt git vim install i3 i3status rofi i3lock xbacklight feh compton lxappearance
+sudo apt install vim tmux xcape i3 i3status rofi i3lock xbacklight feh compton lxappearance
